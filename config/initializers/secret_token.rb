1ae21d2589778faa158cd4a06fff69738ddb7272
minyoung <minyoung9353@gmail.com>
2014-01-16 17:39:53 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhumPub2::Application.config.secret_key_base = 'e0da941dedac1a836e88e8079db0621a192bd2f5870185fb2777945d0527021aaa2a7d1e6bbe5b478e23f74459c7c903be93e9649d8cfa82d3961e2a9d9e0d47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhumPub2::Application.config.session_store :cookie_store, key: '_whum_pub_2_session'
